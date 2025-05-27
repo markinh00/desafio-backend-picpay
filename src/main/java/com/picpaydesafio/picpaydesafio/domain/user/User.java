e6f1402caf1c0501940cd8ab24fd1936e2966442
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-05-27 17:28:07 -0300

    feat: create user domain and repository

---
+++ src/main/java/com/picpaydesafio/picpaydesafio/domain/user/User.java
@@ -0,0 +1,30 @@
+package com.picpaydesafio.picpaydesafio.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
